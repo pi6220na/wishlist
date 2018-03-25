875240373cd81b7d88baa7c62dba88ab1c8fccad
My name <22032408+pi6220na@users.noreply.github.com>
2018-03-25 18:54:15 -0500

    Lab Updates

--- travel_wishlist/models.py
+++ travel_wishlist/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+import time
 
 # Create your models here.
 
 class Place(models.Model):
     name = models.CharField(max_length=200)
     visited = models.BooleanField(default=False)
+    visited_date = models.DateTimeField(
+            blank=True, null=True)
+    review_text = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return '%s visited? %s' % (self.name, self.visited)
+        struct_time = time.strptime(self.visited_date, "%d %b %y")
+        return '%s visited? %s %s %s' % (self.name, self.visited, struct_time, self.review_text)
